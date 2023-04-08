a1fbb1e4b97dc876c6223a4a8e2aca629e2cea34
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-04-09 00:27:21 +0400

    Initial Commit

---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by aplle on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
